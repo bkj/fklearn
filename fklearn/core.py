ca6b35dba90ae7296901f0158c726392cc891959
bkj <ben@gophronesis.com>
2017-02-08 18:44:59 -0500

    adding multi predict

--- fklearn/core.py
+++ fklearn/core.py
@@ -9,10 +9,11 @@ from numba import autojit
 def _sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def _score_l2(X):
+def _score_l2(X, i=0):
     """ 
         Feature counts -> predictions
-        X consists of (count, idf_weight, model_weight) triplets
+        X consists of (count, idf_weight, model_weight_1, model_weight_2, ...) tuples
+        i is a pointer to which model_weight_* you care about
         
         !! Need to test
     """
@@ -20,7 +21,7 @@ def _score_l2(X):
     num = 0
     denom = 0
     for x in X:
-        num += (x[0] * x[1] * x[2])
+        num += (x[0] * x[1] * x[2 + i])
         denom += ((x[0] * x[1]) ** 2)
     
     denom = np.sqrt(denom)
@@ -29,6 +30,9 @@ def _score_l2(X):
 
 sigmoid = autojit(_sigmoid)
 score_l2 = autojit(_score_l2)
+
+# --
+# Model re-implementations
 
 def tfidf_svc_predict(x, lookup):
     ngrams = word_ngrams(x.lower())
@@ -40,3 +44,15 @@ def tfidf_svc_predict(x, lookup):
     
     return sigmoid(score * lookup['calibration_coef'] + lookup['calibration_intercept'])
 
+
+def tfidf_svc_predict_multi(x, lookup):
+    ngrams = word_ngrams(x.lower())
+    weights = [(ngrams.count(z), ) + lookup['coef'][z] for z in set(ngrams) if z in lookup['coef']]
+    
+    scores = copy(lookup['intercept'])
+    if weights:
+        for i in range(len(scores)):
+            scores[i] += score_l2(weights, i)
+            scores[i] = sigmoid(scores[i] * lookup['calibration_coef'][i] + lookup['calibration_intercept'][i])
+    
+    return scores
